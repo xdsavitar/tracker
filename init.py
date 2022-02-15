4dcc2db4b87611673ea8f510469b9a5261453956
xdsavitar <gamingendmore@gmail.com>
2022-02-14 16:28:37 -0800

    bug fix

--- init.py
+++ init.py
@@ -119,12 +119,16 @@ def calculateTime(member):
 
 
 def fetchLast(member):
-    with open(f"vcTEMP/{member}.txt","r") as readVC:
-        print(member)
-        time_spent = readVC.read()
-        elapsed = ElapseTimes(time_spent)
-        return elapsed
+    try:
+        with open(f"vcTEMP/{member}.txt","r") as readVC:
+            print(member)
+            time_spent = readVC.read()
+            elapsed = ElapseTimes(time_spent)
+            return elapsed
 
+    except FileNotFoundError:
+        elapsed = "Unknown"
+        return elapsed
 
 def getUserTSIV(user,server_id):
     collection = MongoDBConnect(server_id)
