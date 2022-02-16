e5cee4405031be66a6718ba9ccdf6f2a4d376d4d
xdsavitar <gamingendmore@gmail.com>
2022-02-16 15:08:01 -0800

    s

--- init.py
+++ init.py
@@ -288,7 +288,7 @@ async def on_message(package):
 
     await client.process_commands(package)
 
-#CORE_KEY = input("CORE_KEY: ")
-client.run("ODQwMDYwNDY0ODI5NTYyODkw.YJStRg.J2vz6jxoNeRvXGEmEuSL1hQ_cOk")
+CORE_KEY = input("CORE_KEY: ")
+client.run(CORE_KEY)
 
 ##
